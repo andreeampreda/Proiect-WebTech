5470252e5854c340cdadf51e0d4b3623d5fe21d2
Daria <popoiudaria22@stud.ase.ro>
2025-01-20 03:08:39 +0200

    Created Add Conference

--- front-end-react/src/components/Conferences/Conferences.jsx
+++ front-end-react/src/components/Conferences/Conferences.jsx
@@ -3,25 +3,65 @@ import "./Conferences.css";
 import { useState, useEffect } from "react";
 import CardConference from "../CardConference/CardConference";
 import ConferenceContent from "../ConferenceContent/ConferenceContent";
+import AddConference from "../AddConference/AddConference";
 
 //Conferences Page components
 
 function Conferences() {
-  const conferenceSpan = "All conferences";
+  const conferenceSpan = "View:";
   const SERVER_URL = "http://localhost:8080/conference";
+  const CONFERENCE_URL = `http://localhost:8080/confManagement/conferences/`;
 
   const [conferences, setConferences] = useState([]);
+  const [userConferences, setUserConferences] = useState([]);
   const [isContentOpen, setIsContentOpen] = useState(false);
   const [selectedConference, setSelectedConference] = useState(null);
+  const [userRole, setUserRole] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  useEffect(() => {
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const fetchAllConferences = () => {
     fetch(`${SERVER_URL}`)
       .then((response) => response.json())
       .then((data) => setConferences(data.conferences))
       .catch((error) => {
         console.log("Couldn't fetch conferences");
       });
+  };
+
+  useEffect(() => {
+    const role = localStorage.getItem("role");
+    setUserRole(role);
+
+    fetchAllConferences();
   }, []);
+
+  const handleOptionChange = (e) => {
+    const userId = localStorage.userId;
+    const selectedOption = e.target.value;
+
+    if (selectedOption == "") {
+      fetchAllConferences();
+      console.log("toate conferinte;e");
+    } else if (selectedOption === "user") {
+      setConferences([]);
+
+      fetch(`${CONFERENCE_URL}${userId}`)
+        .then((response) => response.json())
+        .then((data) => setUserConferences(data)) ///!!!!!!!
+        .catch((error) => {
+          console.log("Couldn't fetch conferences");
+        });
+      console.log("doar ale mele");
+    }
+  };
 
   const handleConferenceClick = (conference) => {
     console.log("click", conference);
@@ -32,11 +72,21 @@ function Conferences() {
 
   const handleCloseContent = () => {
     setIsContentOpen(false);
+    fetchAllConferences();
   };
 
   return (
     <div className="conferencePage">
-      <span className="title">{conferenceSpan}</span>
+      <div className="conferenceUpper">
+        <span className="title">{conferenceSpan}</span>
+        <select className="dropDown" onChange={handleOptionChange}>
+          <option value="">All</option>
+          <option value="user">Only yours</option>
+        </select>
+        {userRole === "organizer" && (
+          <button onClick={handleOpenModal}>Add new Conference</button>
+        )}
+      </div>
       <div className="conferenceContainer">
         {conferences.length > 0 ? (
           conferences.map((conference) => (
@@ -58,6 +108,12 @@ function Conferences() {
           ></ConferenceContent>
         )}
       </div>
+      {isModalOpen && (
+        <AddConference
+          onClose={handleCloseModal}
+          authorId={localStorage.getItem("userId")}
+        />
+      )}
     </div>
   );
 }
